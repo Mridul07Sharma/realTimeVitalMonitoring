772ce70433efe6bde4b88230f3c30776cb0f3f51
Shivam <shivamthalwal@gmail.com>
2023-11-14 02:41:16 +0530

    charts added to the project

--- src/pages/Sensor3.js
+++ src/pages/Sensor3.js
@@ -1,6 +1,48 @@
+import "./sensor3.css";
+import Chart from "react-apexcharts";
+import { useEffect, useState } from "react";
+
 const Sensor3 = () => {
+    const [options, setOptions] = useState({});
+
+    const [series, setSeries] = useState([]);
+
+
+    useEffect(() => {
+        setOptions({
+            chart: {
+              id: "basic-bar"
+            },
+            xaxis: {
+              categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000]
+            }
+        });
+        
+        setSeries([
+            {
+              name: "series 1",
+              data: [30, 40, 45, 50, 49, 60, 70, 91, 100]
+            },
+            {
+                name: "series 2",
+                data: [50, 60, 95, 20, 19, 40, 20, 11]
+              }
+          ]);
+    }, []);
+
     return (<>
-    <h2>Sensor 3</h2>
+    <div className="container">
+        <div className="row">
+            <h2 className="heading-sensor3-sensor text-center">Sensor 3</h2>
+            <p className="text-center sensor3-reading"> <b>Sensor 3 Reading: </b> xyz degree.</p>
+        </div>
+
+        <Chart className="row d-flex justify-content-center" 
+               options={options}
+               series={series}
+               type="line"
+               width="800" />
+    </div>
     </>)
 }
 
--- src/pages/Tempsensor.js
+++ src/pages/Tempsensor.js
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react";
+import "./tempsensor.css";
+
 const TempSensor = () => {
+    const [tempReading, setTempReading] = useState(0);
+
+    useEffect(() => {
+        // Call to backend to update temp;
+        setTempReading(101);
+    }, []);
+
     return (<>
-    <h2>Temp Sensor</h2>
+    <div className="container">
+        <div className="row">
+            <h2 className="heading-temp-sensor text-center">Temp Sensor</h2>
+            <p className="text-center temp-reading"> <b>Temp Reading: </b> {tempReading} degree.</p>
+        </div>
+        
+        { tempReading > 0 ? <>
+            <div className="text-center row temp-detail-indicator">
+                {tempReading > 0 && tempReading <= 98.6 ? <p className="temp-type-text normal">Normal Temperature</p> : <></>}
+                {tempReading > 98.6 && tempReading <= 104 ? <p className="temp-type-text moderate">Moderate Fever</p>: <></>}
+                {tempReading > 104 ? <p className="temp-type-text high">High Fever</p>: <></>}
+            </div>
+        </> : <></>}
+    </div>
     </>)
 }
 
